f517ad7d30cad921602304206c7c5060a0b6205e
xhieu2206 <xhieu94@gmail.com>
2021-06-22 12:06:49 +0700

    Create unit tests for Task Service

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskService } from './task.service';
+import { OrderService } from '../order/order.service';
 
 describe('TaskService', () => {
   let service: TaskService;
+  const mockOrderService = {
+    updateConfirmedOrderStatus: jest.fn(function () {
+      console.log(`Updating confirmed order's status ...`);
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
-    }).compile();
+      providers: [TaskService, OrderService],
+    })
+      .overrideProvider(OrderService)
+      .useValue(mockOrderService)
+      .compile();
 
     service = module.get<TaskService>(TaskService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it(`should call the "updateConfirmedOrderStatus" in handleCron method`, () => {
+    service.handleCron();
+    expect(mockOrderService.updateConfirmedOrderStatus).toHaveBeenCalled();
   });
 });
